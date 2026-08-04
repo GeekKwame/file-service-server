bf3074183af1ddad42a2d9c5f17ea8000876c9eb
Edmund Blessing <dogbeblessingkwame@gmail.com>
2026-08-04 22:04:02 +0000

    fix(backend): migrate dev state backend from local to S3 bucket to share state with GitHub Actions

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,6 +1,8 @@
 terraform {
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "s3" {
+    bucket  = "file-service-terraform-state"
+    key     = "terraform-state/file-service/dev/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
-
